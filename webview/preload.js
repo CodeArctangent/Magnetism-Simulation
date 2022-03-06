a37a4dffd34cb7dc45ac170ed1a859b15f64879e
Sir Code <taylorschneider07@yahoo.com>
2022-03-05 23:16:56 -0500

    Update simulation for 5 dimensional magnets

--- webview/preload.js
+++ webview/preload.js
@@ -89,20 +89,12 @@ window.addEventListener('DOMContentLoaded', () => {
         }, "image/png");
     }); 
     const charges = [
-        new Charge(cv.width - 200, cv.height - 200, 30),
-        new Charge(200, 200, -30)
+        new Charge(0, 0, -10),
+        new Charge(cv.width, cv.height, 10),
+        new Charge(cv.width, 0, -5),
+        new Charge(0, cv.height, 5)
     ]
-    let chgDist = (x, y) => {
-        let dist = 0;
-        for (let c = 0; c < charges.length; ++c) {
-            let chg = charges[c];
-            // let len = Math.sqrt(x+chg.x**2 + y+chg.y**2)-(chg.value/2);
-            // dist = Math.min(dist, len);
-            dist = Math.sqrt(x+chg.x**2 + y+chg.y**2)-(chg.value/2);
-        }
-        return dist;
-    }
-    const res = 20;
+    const res = 8;
     let gridX = new Array(cv.width / res);
     let gridY = new Array(cv.width / res);
     for (let i = 0; i < (cv.width / res); ++i) {
@@ -113,29 +105,50 @@ window.addEventListener('DOMContentLoaded', () => {
         cv.fillStyle = 'black';
         charges[0].x = mouse[0];
         charges[0].y = mouse[1];
+        charges[1].x = -mouse[0] + cv.width;
+        charges[1].y = -mouse[1] + cv.height;
+        charges[2].x = mouse[0];
+        charges[2].y = -mouse[1] + cv.height;
+        charges[3].x = -mouse[0] + cv.width;
+        charges[3].y = mouse[1];
         let final = calculateGrid(gridX, gridY, charges, cv.width, cv.height, res);
         gridX = final[0];
         gridY = final[1];
         cv.cover();  
         // Draw Grid
-        cv.style = 'rgb(50, 50, 50)';
-        for (let x = 0; x < (cv.width / res); ++x) {
-            cv.line(0, res + (res * x), cv.width, res + (res * x));
-            cv.line(res + (res * x), 0, res + (res * x), cv.width);
-        }
+        // cv.style = 'rgb(50, 50, 50)';
+        // for (let x = 0; x < (cv.width / res); ++x) {
+        //     cv.line(0, res + (res * x), cv.width, res + (res * x));
+        //     cv.line(res + (res * x), 0, res + (res * x), cv.width);
+        // }
         // Draw Magnetic Paths
-        for (let m = 0; m < 5000; ++m) {
-            let x = util.randomRange(0, cv.width);
-            let xf = Math.floor(x / res);
-            let y = util.randomRange(0, cv.height);
-            let yf = Math.floor(y / res);
-            // cv.strokeStyle = `rgb(${cv.width/res/x*255}, ${cv.height/res/y*255}, ${cv.width/res/(-x+cv.width)*255})`;
-            // let dist = chgDist(x, y);
-            let gridx = gridX[xf][yf];
-            let gridy = gridY[xf][yf];
-            cv.strokeStyle = `rgb(${gridx*20}, ${gridy*20}, ${Math.hypot(gridx, gridy)*10})`;
-            // cv.strokeStyle = 'white';
-            cv.line(x, y, x + gridx / 2, y + gridy / 2);
+        // for (let m = 0; m < 5000; ++m) {
+        //     let x = util.randomRange(0, cv.width);
+        //     let xf = Math.floor(x / res);
+        //     let y = util.randomRange(0, cv.height);
+        //     let yf = Math.floor(y / res);
+        //     // cv.strokeStyle = `rgb(${cv.width/res/x*255}, ${cv.height/res/y*255}, ${cv.width/res/(-x+cv.width)*255})`;
+        //     // let dist = chgDist(x, y);
+        //     let gridx = gridX[xf][yf];
+        //     let gridy = gridY[xf][yf];
+        //     cv.strokeStyle = `rgb(${gridx*20}, ${gridy*20}, ${Math.hypot(gridx, gridy)*10})`;
+        //     // cv.strokeStyle = 'white';
+        //     cv.line(x, y, x + gridx / 2, y + gridy / 2);
+        // }
+        for (let y = 0; y < cv.height / res; ++y) {
+            for (let x = 0; x < cv.width / res; ++x) {
+                // cv.strokeStyle = `rgb(${cv.width/res/x*255}, ${cv.height/res/y*255}, ${cv.width/res/(-x+cv.width)*255})`;
+                // let dist = chgDist(x, y);
+                // cv.strokeStyle = 'white';
+                let nx = x * res;
+                let ny = y * res;
+                let gridx = gridX[x][y];
+                let gridy = gridY[x][y];
+                // cv.style = `rgb(${gridx*20}, ${gridy*20}, ${(255-gridx*20-gridy*20)})`;
+                cv.style = `rgb(${(gridx+gridy)*20*(Math.sin(time/500)+1)}, ${gridy*20*(Math.cos(time/500)+1)}, ${(255-gridx*20-gridy*20)*(Math.tan(time/500)+1)})`;
+                cv.fillRect(nx, ny, res, res);
+                cv.line(nx - (gridx / 2), ny - (gridy / 2), nx + gridx, ny + gridy);
+            }
         }
         // Draw Charges
         for (let c = 0; c < charges.length; ++c) {
